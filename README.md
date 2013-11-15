7fdb308126f5b09d78fad79331128d8d591c742a
mosampaio <mosampaio@gmail.com>
2013-11-14 21:34:13 -0300

    First version of the simple bf interpreter

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 brainfuck.groovy
 ================
 
-a brainfuck Interpreter written in groovy
+a very simple brainfuck Interpreter written in groovy
+
+(BrainFuck)[http://en.wikipedia.org/wiki/Brainfuck]?
+===============
+
+Brainfuck is an esoteric programming language noted for its extreme minimalism. The language consists of only eight simple commands and an instruction pointer. It is a Turing tarpit, designed to challenge and amuse programmers, and was not made to be suitable for practical use. It was created in 1993 by Urban Müller.
+
+How to use?
+===============
+
+```groovy
+import br.com.mosampaio.bf.BFInterpreter
+//.....
+
+def interpreter = new BFInterpreter()
+interpreter.execute "++.++." //your bf code
+```
---
+++ br/com/mosampaio/bf/BFInterpreter.groovy
@@ -0,0 +1,58 @@
+package br.com.mosampaio.bf
+
+import static Character.MIN_CODE_POINT
+
+class BFInterpreter {
+  def execute(input){
+    def tokens = "<>+-.,[]"
+    def cells = [MIN_CODE_POINT]
+    def pointer = 0
+    def count = 0
+    def whileStack = []
+    while(count < input.size()){
+      if (!tokens.contains(input[count])){
+        count++
+        continue
+      }
+      switch(input[count]) {
+        case ".":
+          println (cells[pointer])
+          break
+        case ">":
+          pointer++
+          if (cells[pointer] == null) cells[pointer] = MIN_CODE_POINT
+          break
+        case "<":
+          if (pointer == 0) throw new NullPointerException("Pointer out of range")
+          break
+        case "+":
+          cells[pointer]++
+          break
+        case "-":
+          cells[pointer]--
+          break
+        case ",":
+          cells[pointer] = System.in.read()
+          break
+        case "[":
+          if (cells[pointer] != 0) {
+            whileStack << count
+          } else {
+            while(count < input.size()) {
+              if (input[count] == ']') break
+              count++
+            }
+          }
+          break
+        case "]":
+          count = whileStack.pop()
+          continue
+          break
+        default: 
+          break
+        }
+        count++
+      }
+    println cells
+  }
+}
